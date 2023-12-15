c56aaa4846c6c438cb5e864f48da370a14b29a4f
EddySlek <slek.eddy@gmail.com>
2023-12-15 00:01:08 -0700

    first commit

---
+++ main.py
@@ -0,0 +1,17 @@
+firstName = "Eddy"
+lastName = "Slek"
+age = 35
+weight = 206.6
+isTall = True
+
+print(firstName)
+
+print(type(age))
+# print(type(isTall))
+
+if age >= 30:
+    print("you are done, dude")
+elif age == 23:
+    print("kop kop")
+else:
+    print("")
